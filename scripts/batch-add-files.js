f31b9d0aab45e919b1dda89ac3a021eb15dcf164
fdiengott <freddy.diengott@proton.me>
2024-12-15 12:41:04 -0500

    generalize script

--- scripts/batch-add-files.js
+++ scripts/batch-add-files.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env zx
+import { $ } from 'zx';
 
 import works from './data/works.js';
 
+const content = works;
+const dir = 'works';
+
+const convertTitleToFilename = (title) =>
+    title
+        .trim()
+        .replace(/["\.?]/g, '')
+        .replace(/\s/g, '-')
+        .toLowerCase();
+
 await Promise.all(
-    works.map(async (work) => {
-        const { filename, content } = work;
-        await $`echo "${content}" > ../src/content/works/${filename}.md`;
+    content.map(async (item) => {
+        const { content } = item;
+        const title = content.match(/title: (.+)/)[1];
+        const filename = convertTitleToFilename(title);
+        await $`echo ${content} > ./src/content/${dir}/${filename}.md`;
     }),
 );

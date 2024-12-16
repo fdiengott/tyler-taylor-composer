f52294ae507c54e737d4240e3ddf42cbb3b1edec
fdiengott <freddy.diengott@proton.me>
2024-12-16 00:00:43 -0500

    add press scripts

---
+++ scripts/data/press.js
@@ -0,0 +1,104 @@
+export default [
+    {
+        content: `
+---
+linkText: 2023 Copland House Residency Awards
+linkUrl: http://www.coplandhouse.org/media/Copland-House-Residency-Awards-2023-release.pdf
+publication: Elizabeth Dworkin
+---
+            `,
+    },
+    {
+        content: `
+---
+linkText: Embracing Its Audience, A Visionary Orchestra Breaks Cultural MoldBlockbuster Commissions
+publication: Classical Voice North America – Nancy Maltiz
+---
+`,
+    },
+    {
+        content: `
+---
+linkText: Blockbuster Commissions
+publication: Symphony.org
+---
+`,
+    },
+    {
+        content: `
+---
+linkText: TYLER TAYLOR revises the love story between saxophones and orchestras
+publication: Louisville Public Media
+---
+`,
+    },
+    {
+        content: `
+---
+linkText: KENTUCKY FRESH (LOUISVILLE ORCHESTRA)
+linkUrl: https://arts-louisville.com/2022/09/20/kentucky-fresh-louisville-orchestra/
+publication: Arts-Louisville.com, a review by Annette Skaggs
+---
+`,
+    },
+    {
+        content: `
+---
+linkText: TEDDY ABRAMS & LOUISVILLE ORCHESTRA ANNOUNCE INAUGURAL GROUP OF COMPOSERS FOR NEW “CREATORS CORPS” RESIDENCY PROGRAM BEGINNING SEP 1
+linkUrl: https://wfpl.org/louisville-orchestra-names-first-cohort-of-its-composer-residency-creators-corps/
+publication: Louisville Orchestra
+---
+`,
+    },
+    {
+        content: `
+---
+linkText: Louisville Orchestra names first cohort of its composer residency, ‘Creators Corps’
+---
+`,
+    },
+    {
+        content: `
+---
+linkUrl: https://wfpl.org/louisville-orchestra-names-first-cohort-of-its-composer-residency-creators-corps/
+publication: WFPL
+---
+`,
+    },
+    {
+        content: `
+---
+linkText: How these composers plan to alter 'the creative fabric' of Louisville through new music
+linkUrl: https://www.courier-journal.com/story/entertainment/music/2022/07/25/louisville-orchestra-creator-corps-tyler-taylor-lisa-bielawa-tj-cole/65381775007/
+publication: Courier Journal
+---
+`,
+    },
+    {
+        content: `
+        ---
+        linkText: Louisville Orchestra announces inaugural group of composers for new residency program
+linkUrl: https://www.wdrb.com/wdrb-in-the-morning/louisville-orchestra-announces-inaugural-group-of-composers-for-new-residency-program/article_6c1beb0c-0cbc-11ed-b84d-076dc4229152.html
+publication: WDRB News
+        ---
+        `,
+    },
+    {
+        content: `
+        ---
+        linkText: The Louisville Orchestra Announces Inaugural Creator Corps
+linkUrl: https://www.leoweekly.com/2022/07/the-louisville-orchestra-announces-inaugural-creator-corps/
+        publication: LEO Weekly
+        ---
+        `,
+    },
+    {
+        content: `
+        ---
+        linkText: Kentucky’s Louisville Orchestra Launches Inaugural Composers Program
+linkUrl: https://theviolinchannel.com/kentuckys-louisville-orchestra-names-participants-in-inaugural-composers-program/
+publication: The Violin Channel
+        ---
+        `,
+    },
+];
